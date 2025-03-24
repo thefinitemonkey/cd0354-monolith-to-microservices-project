c92b41f018a5a55ae226b093149ad40f6a507aa9
Doug Brown <dougb@finitemonkey.com>
2025-03-24 16:10:24 -0500

    Fixes for project to build and run properly on current version of Node

--- udagram-api-feed/src/sequelize.ts
+++ udagram-api-feed/src/sequelize.ts
@@ -3,17 +3,17 @@ import {config} from './config/config';
 
 
 export const sequelize = new Sequelize({
-  'username': config.username,
-  'password': config.password,
-  'database': config.database,
-  'host': config.host,
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host,
 
-  'dialect': config.dialect,
+  dialect: 'postgres',
   dialectOptions: {
     ssl: {
       require: true,
       rejectUnauthorized: false 
     }
   },
-  'storage': ':memory:',
+  storage: ':memory:',
 });
