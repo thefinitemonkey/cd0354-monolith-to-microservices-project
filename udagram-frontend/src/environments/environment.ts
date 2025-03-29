40d8869a58a8608bd872c6ccc532d7c47196d568
Doug Brown <dougb@finitemonkey.com>
2025-03-29 15:59:24 -0500

    Update for frontend environment file to use in k8s

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a2386f9278bea4bc68bdfc872acd8cfe-1406046176.us-east-1.elb.amazonaws.com/api/v0'
 };
 
 /*
