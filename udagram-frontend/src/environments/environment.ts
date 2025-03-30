6932818308ff60220e1bf199186419b7f2e470b3
Doug Brown <dougb@finitemonkey.com>
2025-03-30 15:46:27 -0500

    More k8s deployment debugging

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://afcf71dca073a48a49f6ab0ff3d632ff-2138048085.us-east-1.elb.amazonaws.com/api/v0'
+  apiHost: 'http://afcf71dca073a48a49f6ab0ff3d632ff-2138048085.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
